26cdbd60d0955a36dda1e5b111f8bd15b61ab63b
mboultoureau <contact@mboultoureau.com>
2023-04-26 11:21:00 +0200

    fix: audio lib

--- src/SoundDevice.cpp
+++ src/SoundDevice.cpp
@@ -1,36 +1,62 @@
 #include "SoundDevice.h"
 #include <AL/al.h>
 #include <AL/alc.h>
-#include <stdio.h>
+#include <iostream>
 
-SoundDevice* SoundDevice::get() {
+SoundDevice* SoundDevice::get()
+{
     static  SoundDevice* snd_device = new SoundDevice();
     return snd_device;
 }
 
-SoundDevice::SoundDevice() {
+SoundDevice::SoundDevice()
+{
     p_ALCDevice = alcOpenDevice(nullptr); //nullptr : will get the default device
     if (!p_ALCDevice)
-        throw ("failed to get sound device");
+    {
+        std::cout << "OpenAL: failed to get sound device" << std::endl;
+        return;
+    }
+
     p_ALCContext = alcCreateContext(p_ALCDevice, nullptr); //create context
     if (!p_ALCContext)
-        throw ("failed to set sound context");
+    {
+        std::cout << "OpenAL: failed to create context" << std::endl;
+        return;
+    }
+
     if (!alcMakeContextCurrent(p_ALCContext)) //make context current
-        throw ("failed to make context current");
+    {
+        std::cout << "OpenAL: failed to make context current" << std::endl;
+        return;
+    }
 
     const ALCchar* name = nullptr;
     if (alcIsExtensionPresent(p_ALCDevice,"ALC_ENUMERATE_ALL_EXT"))
         name = alcGetString(p_ALCDevice, ALC_ALL_DEVICES_SPECIFIER);
     if (!name || alcGetError(p_ALCDevice) != AL_NO_ERROR)
         name = alcGetString(p_ALCDevice, ALC_DEVICE_SPECIFIER);
-    printf("Opened\"%s\"\n",name);
+
+    std::cout << "OpenAL: opened device " << name << std::endl;
 }
-SoundDevice::~SoundDevice() {
-    if (!alcMakeContextCurrent(nullptr))
-        throw ("failed to set context to nullptr");
+
+SoundDevice::~SoundDevice()
+{
+    if (!alcMakeContextCurrent(nullptr)) {
+        std::cout << "OpenAL: failed to unset context" << std::endl;
+        return;
+    }
+
     alcDestroyContext(p_ALCContext);
     if (p_ALCContext)
-        throw ("failed to unset during close");
+    {
+        std::cout << "OpenAL: failed to destroy context" << std::endl;
+        return;
+    }
+
     if (!alcCloseDevice(p_ALCDevice))
-        throw ("failed to close sound device");
+    {
+        std::cout << "OpenAL: failed to close sound device" << std::endl;
+        return;
+    }
 }
